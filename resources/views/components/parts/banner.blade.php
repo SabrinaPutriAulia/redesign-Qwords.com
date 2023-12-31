16cead4d60f62b3421aed5dfa33023445b506cf8
SabrinaPutriAulia <21081010048@student.upnjatim.ac.id>
2023-12-31 21:45:59 +0700

    first: commit

---
+++ resources/views/components/parts/banner.blade.php
@@ -0,0 +1,38 @@
+<div class="container-lg pt-5">
+    <div class="d-flex">
+        <div class="justify-content-center pt-5">
+            <p class="fs-1 fw-bold text-center" style="color: black">
+                Cloud Hosting untuk <br> <span class="text-custom-purple">Kesuksesan Websitemu!</span>
+            </p>
+            <div class=" m-auto justify-item-center" style="width: 55%">
+                <p class="text-center">Qwords menawarkan layanan Cloud Hosting yang Cepat, Aman,
+                    dan Dukungan 24 jam
+                    untuk memenuhi
+                    kebutuhan
+                    hosting Anda. Tersertifikasi ISO 27001 dengan Data Center Tier 3.</p>
+            </div>
+            <div class="m-auto text-center mt-5">
+                <button
+                    class="btn rounded-pill py-2 px-4 fw-bold btn-custom-orange shadow-lg p-3 mb-5 rounded btn-custom-orange">Get
+                    Started <i class='bx bx-right-arrow-alt'></i></button>
+            </div>
+            <p class="fs-5 fw-semibold text-center">
+                Telah Dipercaya oleh 45.000+ Pelanggan Aktif
+            </p>
+            <div class="mx-auto text-center my-5">
+                <img src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/partner-customer/Pelanggan/Cimory.webp"
+                    alt="" style="max-height: 90px">
+                <img src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/partner-customer/Pelanggan/Forisa.webp"
+                    alt="" style="max-height: 90px">
+                <img src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/partner-customer/Pelanggan/Elsheskin.webp"
+                    alt="" style="max-height: 90px">
+                <img src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/partner-customer/Pelanggan/Icon.webp"
+                    alt="" style="max-height: 90px">
+                <img src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/partner-customer/Pelanggan/Horison.webp"
+                    alt="" style="max-height: 90px">
+                <img src="https://qwords.com/v2/wp-content/themes/qwords/assets/images/icons/partner-customer/Pelanggan/MD.webp"
+                    alt="" style="max-height: 90px">
+            </div>
+        </div>
+    </div>
+</div>
---
+++ resources/views/components/parts/service.blade.php
@@ -0,0 +1,330 @@
+<div class="bg-color-grey">
+    <div class="container-lg py-5">
+        <div class="justify-content-center mb-5">
+            <p class="fs-1 fw-bold text-center text-black">
+                Layanan Kami
+            </p>
+            <div class=" m-auto text-center" style="width: 65%">
+                <p>Tersedia untuk memenuhi segala kebutuhan digital Anda, layanan kami tidak sekadar menyediakan
+                    hosting,
+                    melainkan menghadirkan pengalaman digital yang luar biasa. Dari solusi cloud hosting yang andal
+                    hingga
+                    layanan personalisasi yang inovatif, kami berkomitmen untuk merampingkan perjalanan online Anda.
+                </p>
+            </div>
+
+            <div class="card mb-3 mt-5 border border-0 rounded-5">
+                <div class="row g-1 p-4">
+                    <div class="col-md-5 p-5">
+                        <div class="d-flex row align-items-center h-100" style="width: 100%">
+                            <div>
+                                <p class="fs-2 fw-bold my-4">Cloud Hosting Indonesia Diskon Hingga 50%</p>
+                                <p class="card-text my-4">Layanan cloud hosting Indonesia terbaik dengan teknologi andal
+                                    untuk website Anda menjadikan pengelolaan online lebih mudah dan efisien. Dengan
+                                    infrastruktur terkini yang kami miliki, kami menghadirkan kinerja tinggi dan
+                                    kestabilan untuk memenuhi tuntutan website modern</p>
+                                <button class="btn rounded-pill py-2 px-4 fw-bold btn-custom-grey my-4">Learn
+                                    more</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-md-7">
+                        <div class="card-body">
+                            <div class="row row-cols-1 row-cols-md-2 g-4">
+                                <div class="col">
+                                    <div class="card">
+                                        <div class="card-body">
+
+                                            <p class="card-text">Value Cloud Hosting</p>
+                                            <h5 class="card-title fw-bold">VCH1</h5>
+                                            <div class="py-3">
+                                                <div class="d-flex">
+                                                    <p>
+                                                        <i class='bx bx-xs bx-hdd text-custom-orange'></i>
+                                                    </p>
+                                                    <p class="ms-2 small">
+                                                        Disk <strong>3 GB</strong>
+                                                    </p>
+                                                </div>
+
+                                                <div class="d-flex">
+                                                    <p>
+                                                        <i class='bx bx-xs bx-wifi text-custom-orange'></i>
+                                                    </p>
+                                                    <p class="ms-2 small">
+                                                        Bandwith <strong>Unlimited</strong>
+                                                    </p>
+                                                </div>
+
+                                                <div class="d-flex">
+                                                    <p>
+                                                        <i class='bx bx-xs bx-bolt-circle text-custom-orange'></i>
+                                                    </p>
+                                                    <p class="ms-2 small">
+                                                        CPU <strong>0.05 Core</strong>
+                                                    </p>
+                                                </div>
+
+                                                <div class="d-flex">
+                                                    <p>
+                                                        <i class='bx bx-xs bx-check-shield text-custom-orange'></i>
+                                                    </p>
+                                                    <p class="ms-2 small">
+                                                        <strong>No</strong> Addon Domain
+                                                    </p>
+                                                </div>
+
+                                                <button class="btn fw-semibold btn-custom-orange rounded-4 w-100">Pesan
+                                                    Sekarang</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col">
+                                    <div class="card">
+                                        <div class="card-body">
+                                            <p class="card-text">Unlimited Hosting</p>
+                                            <h5 class="card-title fw-bold">Signature</h5>
+                                            <div class="py-3">
+                                                <div class="d-flex">
+                                                    <p>
+                                                        <i class='bx bx-xs bx-hdd text-custom-orange'></i>
+                                                    </p>
+                                                    <p class="ms-2 small">
+                                                        Disk <strong>Unlimited</strong>
+                                                    </p>
+                                                </div>
+
+                                                <div class="d-flex">
+                                                    <p>
+                                                        <i class='bx bx-xs bx-wifi text-custom-orange'></i>
+                                                    </p>
+                                                    <p class="ms-2 small">
+                                                        Bandwith <strong>Unlimited</strong>
+                                                    </p>
+                                                </div>
+
+                                                <div class="d-flex">
+                                                    <p>
+                                                        <i class='bx bx-xs bx-bolt-circle text-custom-orange'></i>
+                                                    </p>
+                                                    <p class="ms-2 small">
+                                                        CPU <strong>1 Core</strong>
+                                                    </p>
+                                                </div>
+
+                                                <div class="d-flex">
+                                                    <p>
+                                                        <i class='bx bx-xs bx-check-shield text-custom-orange'></i>
+                                                    </p>
+                                                    <p class="ms-2 small">
+                                                        <strong>5/5</strong> Addon Domain
+                                                    </p>
+                                                </div>
+
+
+                                                <button class="btn fw-semibold btn-custom-orange rounded-4 w-100">Pesan
+                                                    Sekarang</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col">
+                                    <div class="card">
+
+                                        <div class="card-body">
+                                            <p class="card-text">HPCH Bisnis 1</p>
+                                            <h5 class="card-title fw-bold">High Performance</h5>
+                                            <div class="py-3">
+                                                <div class="d-flex">
+                                                    <p>
+                                                        <i class='bx bx-xs bx-hdd text-custom-orange'></i>
+                                                    </p>
+                                                    <p class="ms-2 small">
+                                                        Disk <strong>3 GB</strong>
+                                                    </p>
+                                                </div>
+
+                                                <div class="d-flex">
+                                                    <p>
+                                                        <i class='bx bx-xs bx-wifi text-custom-orange'></i>
+                                                    </p>
+                                                    <p class="ms-2 small">
+                                                        Bandwith <strong>Unlimited</strong>
+                                                    </p>
+                                                </div>
+
+                                                <div class="d-flex">
+                                                    <p>
+                                                        <i class='bx bx-xs bx-bolt-circle text-custom-orange'></i>
+                                                    </p>
+                                                    <p class="ms-2 small">
+                                                        CPU <strong>1 Core</strong>
+                                                    </p>
+                                                </div>
+
+                                                <div class="d-flex">
+                                                    <p>
+                                                        <i class='bx bx-xs bx-check-shield text-custom-orange'></i>
+                                                    </p>
+                                                    <p class="ms-2 small">
+                                                        <strong>No</strong> Addon Domain
+                                                    </p>
+                                                </div>
+
+
+                                                <button class="btn fw-semibold btn-custom-orange rounded-4 w-100">Pesan
+                                                    Sekarang</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col">
+                                    <div class="card">
+
+                                        <div class="card-body">
+                                            <p class="card-text">VPS SC1</p>
+                                            <h5 class="card-title fw-bold">Cloud VPS KVM SSD</h5>
+                                            <div class="py-3">
+                                                <div class="d-flex">
+                                                    <p>
+                                                        <i class='bx bx-xs bx-hdd text-custom-orange'></i>
+                                                    </p>
+                                                    <p class="ms-2 small">
+                                                        Disk <strong>25 GB</strong>
+                                                    </p>
+                                                </div>
+
+                                                <div class="d-flex">
+                                                    <p>
+                                                        <i class='bx bx-xs bx-wifi text-custom-orange'></i>
+                                                    </p>
+                                                    <p class="ms-2 small">
+                                                        Bandwith <strong>Unlimited</strong>
+                                                    </p>
+                                                </div>
+
+                                                <div class="d-flex">
+                                                    <p>
+                                                        <i class='bx bx-xs bx-bolt-circle text-custom-orange'></i>
+                                                    </p>
+                                                    <p class="ms-2 small">
+                                                        CPU <strong>1 Core</strong>
+                                                    </p>
+                                                </div>
+
+                                                <div class="d-flex">
+                                                    <p>
+                                                        <i class='bx bx-xs bx-check-shield text-custom-orange'></i>
+                                                    </p>
+                                                    <p class="ms-2 small">
+                                                        <strong>Unlimited</strong> Addon Domain
+                                                    </p>
+                                                </div>
+
+
+                                                <button class="btn fw-semibold btn-custom-orange rounded-4 w-100">Pesan
+                                                    Sekarang</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+
+        <div class="row">
+            <div class="col-sm-6 mb-3 mb-sm-0">
+                <div class="card rounded-5 border-0 p-4">
+                    <div class="px-4">
+                        <p class="fs-2 fw-bold my-3">Optimasi Performa Website</p>
+                        <p class="card-text my-3">Optimalkan performa website bisnis Anda dengan berbagai layanan
+                            berkualitas dari Qwords</p>
+
+                    </div>
+                    <div class="card-body">
+                        <div class="row row-cols-1 row-cols-md-2 g-4">
+                            <div class="col">
+                                <div class="card">
+                                    <img src="..." class="card-img-top" alt="...">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Card title</h5>
+                                        <p class="card-text">This is a longer card with supporting text below as a
+                                            natural lead-in to additional content. This content is a little bit longer.
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div class="card">
+                                    <img src="..." class="card-img-top" alt="...">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Card title</h5>
+                                        <p class="card-text">This is a longer card with supporting text below as a
+                                            natural lead-in to additional content. This content is a little bit longer.
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col">
+                                <div class="card">
+                                    <img src="..." class="card-img-top" alt="...">
+                                    <div class="card-body">
+                                        <h5 class="card-title">Card title</h5>
+                                        <p class="card-text">This is a longer card with supporting text below as a
+                                            natural lead-in to additional content.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col d-flex justify-content-center align-items-center">
+                                <button class="btn rounded-pill py-2 px-4 fw-bold btn-custom-grey my-3">Learn
+                                    more</button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6">
+                <div class="card rounded-5 border-0 p-4">
+                    <div class="px-4">
+                        <p class="fs-2 fw-bold my-3">Solusi Paket Hosting Terbaik</p>
+                        <p class="card-text my-3">Kami menawarkan solusi paket hosting terbaik untuk segala kebutuhan
+                            website dan sistem Anda.</p>
+                    </div>
+                    <div class="card-body">
+                        <div class="card mb-3">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">This is a wider card with supporting text below as a natural
+                                    lead-in to additional content. This content is a little bit longer.</p>
+                                </p>
+                            </div>
+                        </div>
+                        <div class="card mb-3">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">This is a wider card with supporting text below as a natural
+                                    lead-in to additional content. This content is a little bit longer.</p>
+                                </p>
+                            </div>
+                        </div>
+                        <div class="card">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">This is a wider card with supporting text below as a natural
+                                    lead-in to additional content. This content is a little bit longer.</p>
+                                </p>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
